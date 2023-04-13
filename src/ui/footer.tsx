b9a7c1f03ab6e883b13e899306e1cb04225edd00
Babul Akter <babulakterfsd@gmail.com>
2023-04-14 01:42:34 +0600

    files and folder setup modified

--- src/ui/footer.tsx
+++ src/ui/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <div className="text-xl text-center py-4">
-      <h6 className="bg-purple-600 text-white">
+    <div className="text-xl text-center">
+      <h6 className="bg-purple-600 text-white py-4">
         Made with love by Babul Akter
       </h6>
     </div>
